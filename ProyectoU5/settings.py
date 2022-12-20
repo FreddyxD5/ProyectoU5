3af2bf3f5ad7cd13b4091cec2aa4854acf4aeee9
FreddyxD5 <freddyxd5@gmail.com>
2022-12-20 17:28:23 -0500

    added mysq

--- ProyectoU5/settings.py
+++ ProyectoU5/settings.py
@@ -104,12 +104,15 @@ WSGI_APPLICATION = 'ProyectoU5.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default': { 
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER':'root',
+        'PASSWORD':'D8xVbogfuyu8L9xwpGG1',
+        'HOST':'containers-us-west-125.railway.app',
+        'PORT':5863,
     }
 }
-
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
