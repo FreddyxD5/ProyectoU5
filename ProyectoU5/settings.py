413fe5cb76b961742fc3dfa1096e9d8c3c34f916
FreddyxD5 <freddyxd5@gmail.com>
2022-12-20 17:08:49 -0500

    fix

--- ProyectoU5/settings.py
+++ ProyectoU5/settings.py
@@ -219,6 +219,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+CSRF_TRUSTED_ORIGINS = ['https://proyectou4-production.up.railway.app']
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
